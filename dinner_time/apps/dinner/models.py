3417c7aebdf8db3edeebea6e9cd4d9f1aa115fd8
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-03-27 21:30:53 +0300

    :card_file_box: create decorator for search and get full cost dinner

--- dinner_time/apps/dinner/models.py
+++ dinner_time/apps/dinner/models.py
@@ -21,6 +21,15 @@ class DinnerOrder(Model):
     create_date = DateTimeField(auto_now_add=True, auto_now=False, null=True, blank=True, verbose_name='Создано')
     update_date = DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Обновлено')
 
+    @property
+    def get_full_cost(self):
+        full_cost = 0
+        all_dinner = DinnerOrder.objects.all()
+        for obj in all_dinner:
+            full_cost += obj.dish.cost
+
+        return full_cost
+
     class Meta:
         verbose_name = "Неодобренное меню"
         verbose_name_plural = "Неодобренное меню"
@@ -29,16 +38,48 @@ class DinnerOrder(Model):
 class MenuGroup(Model):
     name = CharField(max_length=40, blank=True, null=True, verbose_name='Название группы')
 
+    def __str__(self):
+        return self.name
+
 
 class Dish(Model):
     name = CharField(max_length=40, blank=True, null=True, verbose_name='Название блюда')
     cost = FloatField(unique=False, verbose_name='Цена')
-    weight = FloatField(unique=False, verbose_name='Калории')
+    weight = FloatField(unique=False, verbose_name='Вес')
     composition = CharField(max_length=120, blank=True, null=True, verbose_name='Состав')
     menu_group = ForeignKey(MenuGroup, on_delete=PROTECT, related_name='menu_group', verbose_name='Группа меню',
                             blank=True, null=True)
     added_dish = ForeignKey('self', on_delete=CASCADE, related_name='additional_menu',
                             verbose_name='Дополнительное блюдо', blank=True, null=True)
+
+    @classmethod
+    def search(cls, name: str,
+               queryset=None):
+        """
+        Метод поиска по объектам класса Dish
+
+        @param name: (str) Искомое значение
+                Строка в любом регистре
+        @param queryset: (QuerySet) Использовать готовый queryser для поиска в диапазоне переданной выборки
+
+        @return: QuerySet
+        """
+
+        name = str(name).title()
+
+        """
+            Если не передан QuerySet то поиск производится по всем блюдам
+        """
+        if not queryset:
+            queryset = cls.objects.all()
+
+        """
+            Поиск будет производится только среди моделей определенного блюда
+        """
+        if name:
+            queryset = queryset.filter(name=name)
+
+        return queryset
 
     class Meta:
         verbose_name = "Блюдо"

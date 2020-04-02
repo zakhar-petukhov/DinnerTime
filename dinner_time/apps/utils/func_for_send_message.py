1a9018ddf00c32afe2933b4b4008ea7d9773df0f
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-02 08:33:58 +0300

    :pencil2: delete space

--- dinner_time/apps/utils/func_for_send_message.py
+++ dinner_time/apps/utils/func_for_send_message.py
@@ -1,4 +1,4 @@
-def  send_message_for_change_auth_data(url, company_name):
+def send_message_for_change_auth_data(url, company_name):
     header = '''Изменения логина и пароля при регистрации компании.'''
 
     body = f'''

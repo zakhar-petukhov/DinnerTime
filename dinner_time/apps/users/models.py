84dc22d4a6c80506da093b5cc51001a6f623c4ed
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-04 21:19:14 +0300

    :card_file_box: create func for reform phone number, create department column in User and rename column in Department

--- dinner_time/apps/users/models.py
+++ dinner_time/apps/users/models.py
@@ -1,3 +1,4 @@
+import phonenumbers
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.db.models import *
 from mptt.fields import TreeForeignKey
@@ -14,9 +15,12 @@ class User(AbstractUser, MPTTModel):
     last_name = CharField(max_length=20, null=True, blank=True, verbose_name='Фамилия')
     middle_name = CharField(max_length=20, null=True, blank=True, verbose_name='Отчество')
 
-    phone = CharField(max_length=18, null=True, blank=True, verbose_name='Номер телефона')
+    phone = CharField(max_length=12, null=True, blank=True, verbose_name='Номер телефона')
     email = EmailField(max_length=30, null=True, blank=True, verbose_name='Email')
     email_verified = BooleanField(default=False, verbose_name='Email подтвержден')
+
+    department = ForeignKey('users.Department', on_delete=PROTECT, related_name='department_user', blank=True,
+                            null=True, verbose_name='Департамент')
 
     is_blocked = BooleanField(default=False, verbose_name='Заблокирован')
     block_date = DateTimeField(null=True, blank=True, verbose_name='Дата блокироваки')
@@ -34,6 +38,11 @@ class User(AbstractUser, MPTTModel):
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
+    # Проеобразуем номер телефона в общий формат
+    def get_phone_number(self, phone):
+        phone_number = phonenumbers.parse(phone, "RU")
+        return f"+{str(phone_number.country_code) + str(phone_number.national_number)}"
+
     @staticmethod
     def autocomplete_search_fields():
         return 'id', 'username', 'last_name', 'first_name', 'phone'
@@ -45,8 +54,11 @@ class User(AbstractUser, MPTTModel):
 
 class Department(Model):
     name = CharField(max_length=20, null=True, blank=True, verbose_name='Название')
-    employee = ForeignKey(User, on_delete=PROTECT, related_name='employee_department', blank=True, null=True,
-                          verbose_name='Работники')
+    company = ForeignKey('users.User', on_delete=PROTECT, related_name='company_department', blank=True,
+                         null=True, verbose_name='Компания')
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         verbose_name = 'Департамент'

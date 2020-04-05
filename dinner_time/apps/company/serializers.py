22a7ceffb9833f8121064fb8a123b95f89f64590
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-05 17:08:18 +0300

    :art: refactoring serializers, create CompanyDetailSerializer

--- dinner_time/apps/company/serializers.py
+++ dinner_time/apps/company/serializers.py
@@ -1,9 +1,14 @@
+from datetime import datetime
+
+import pytz
+from django.conf import settings
 from rest_framework import serializers
 
-from apps.users.models import User
+from apps.users.models import User, Department
+from users.serializers import DepartmentSerializer
 
 
-class GetInformationCompanySerializer(serializers.ModelSerializer):
+class CompanySerializer(serializers.ModelSerializer):
     """
     A serializer for get all information about company
     """
@@ -13,25 +18,52 @@ class GetInformationCompanySerializer(serializers.ModelSerializer):
         fields = ['id', 'company_name', 'first_name', 'last_name', 'middle_name', 'phone', 'email', 'is_blocked']
 
 
-class CreateCompanySerializer(serializers.ModelSerializer):
+class CompanyBlockSerializer(serializers.ModelSerializer):
+    """
+    A serializer which blocks or unlocks the company
+    """
+
+    is_blocked = serializers.BooleanField(initial=False, label='Заблокирован')
+    block_date = serializers.SerializerMethodField('get_block_date', label='Дата блокировки')
+
+    def get_block_date(self, obj):
+        if obj.is_blocked:
+            return datetime.now(pytz.timezone(settings.TIME_ZONE))
+        return None
+
+    class Meta:
+        model = User
+        fields = ['is_blocked', 'block_date']
+
+
+class CompanyCreateSerializer(serializers.ModelSerializer):
     """
     A serializer for create company in admin panel
     """
 
     class Meta:
         model = User
-        fields = (
-            'id', 'email', 'company_name', 'create_date', 'is_company')
+        fields = ('id', 'email', 'company_name', 'create_date', 'is_company')
 
 
-class ChangeRegAuthDataSerializer(serializers.ModelSerializer):
+class CompanyDetailSerializer(serializers.ModelSerializer):
     """
-    A serializer for update username and password when clicking on the registration link
+    A serializer for get detail information about company
     """
 
-    username = serializers.CharField(required=True)
-    password = serializers.CharField(required=True)
+    department = serializers.SerializerMethodField('get_all_department', label='Все отделы')
+    all_person = serializers.SerializerMethodField('get_all_person', label='Все сотрудники')
+
+    def get_all_department(self, obj):
+        qs = Department.objects.filter(company_id=obj.id)
+        serializer = DepartmentSerializer(instance=qs, many=True)
+        return serializer.data
+
+    def get_all_person(self, obj):
+        return len(User.objects.filter(parent=obj.id))
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'middle_name', 'phone', 'username', 'password')
+        fields = (
+            'id', 'company_name', 'first_name', 'last_name', 'middle_name', 'phone', 'email', 'is_blocked',
+            'department', 'all_person')

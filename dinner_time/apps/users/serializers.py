10e2b5ece3f40a0a01dbbf62aa0e5b2d30148a7a
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-05 09:26:27 +0300

    :art: update method for create department and add user

--- dinner_time/apps/users/serializers.py
+++ dinner_time/apps/users/serializers.py
@@ -1,4 +1,3 @@
-import phonenumbers
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
@@ -9,49 +8,39 @@ User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     """
-    A user serializer for get information
+    Main user serializer for get information
     """
-    phone = serializers.SerializerMethodField('get_phone_number')
-
-    def get_phone_number(self, obj):
-        phone_number = phonenumbers.parse(obj.phone, "RU")
-        return f"+{str(phone_number.country_code) + str(phone_number.national_number)}"
 
     class Meta:
         model = User
         fields = ['id', 'last_login', 'is_superuser', 'username', 'is_staff', 'is_active', 'date_joined', 'first_name',
-                  'last_name', 'middle_name', 'phone', 'email', 'email_verified', 'employee_department', 'is_blocked',
+                  'last_name', 'middle_name', 'phone', 'email', 'email_verified', 'department', 'is_blocked',
                   'block_date', 'create_date', 'update_date', 'lft', 'rght', 'tree_id', 'level', 'parent', 'groups',
                   'user_permissions']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for create user
+    """
+
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'middle_name', 'phone', 'email', 'department']
 
 
 class DepartmentSerializer(serializers.ModelSerializer):
     """
     Serializer for department. Used how main serializer and for create department.
     """
-    name = serializers.CharField(required=True)
+    total_number_users = serializers.SerializerMethodField('get_total_number_users')
+
+    def get_total_number_users(self, obj):
+        return len(User.objects.filter(department=obj.id))
 
     class Meta:
         model = Department
-        fields = ['id', 'name', 'employee']
-
-
-class DepartmentUpdateUserSerializer(DepartmentSerializer):
-    """
-    Update department (add, delete employee)
-    """
-    employee = UserSerializer(many=True)
-
-
-class DepartmentGetSerializer(DepartmentSerializer):
-    """
-    Get all information about department
-    """
-
-    employee = serializers.SerializerMethodField('get_total_number_users')
-
-    def get_total_number_users(self, obj):
-        return len(Department.objects.filter(employee_id=obj.id))
+        fields = ['id', 'name', 'company', 'total_number_users']
 
 
 class EmptySerializer(serializers.Serializer):
--- dinner_time/apps/users/views.py
+++ dinner_time/apps/users/views.py
@@ -1,9 +1,11 @@
 from django.core.exceptions import ImproperlyConfigured
 from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from apps.authentication.utils import create_user_account, generate_random_username_password
 from apps.users.permissions import IsCompanyAuthenticated
 from apps.users.serializers import *
 
@@ -15,21 +17,33 @@ class UserView(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class DepartmentViewSet(GenericViewSet, mixins.UpdateModelMixin, mixins.ListModelMixin):
+class DepartmentViewSet(GenericViewSet, mixins.ListModelMixin):
     permission_classes = [IsCompanyAuthenticated]
     serializer_class = EmptySerializer
     serializer_classes = {
-        'create': DepartmentSerializer,
-        'update': DepartmentUpdateUserSerializer,
-        'list': DepartmentGetSerializer,
+        'create_department': DepartmentSerializer,
+        'add_user': UserCreateSerializer,
+        'list': DepartmentSerializer,
     }
 
-    def create(self, request, *args, **kwargs):
-        request.data['employee'] = request.user.id
+    # Create department
+    @action(methods=['POST'], detail=False)
+    def create_department(self, request, *args, **kwargs):
+        request.data['company'] = request.user.id
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    # Create and add user into department
+    @action(methods=['POST'], detail=False)
+    def add_user(self, request):
+        parent = request.user
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data.update(generate_random_username_password())
+        create_user_account(parent=parent, **serializer.validated_data)
+        return Response(status=status.HTTP_201_CREATED)
 
     def get_serializer_class(self):
         if not isinstance(self.serializer_classes, dict):
@@ -48,4 +62,4 @@ class DepartmentViewSet(GenericViewSet, mixins.UpdateModelMixin, mixins.ListMode
 
     def get_queryset(self):
         employee_id = self.request.user.id
-        return Department.objects.filter(employee_id=employee_id)
+        return Department.objects.filter(company_id=employee_id)

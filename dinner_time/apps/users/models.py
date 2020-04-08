0caacd15378611d559857cf11dad74db57fff204
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-08 16:39:25 +0300

    :card_file_box: company, department models moved from user into company

--- dinner_time/apps/users/models.py
+++ dinner_time/apps/users/models.py
@@ -19,14 +19,14 @@ class User(AbstractUser, MPTTModel):
     email = EmailField(max_length=30, null=True, blank=True, verbose_name='Email')
     email_verified = BooleanField(default=False, verbose_name='Email подтвержден')
 
-    department = ForeignKey('users.Department', on_delete=PROTECT, related_name='department_user', blank=True,
+    department = ForeignKey('company.Department', on_delete=PROTECT, related_name='department_user', blank=True,
                             null=True, verbose_name='Департамент')
 
     is_blocked = BooleanField(default=False, verbose_name='Заблокирован')
     block_date = DateTimeField(null=True, blank=True, verbose_name='Дата блокироваки')
 
-    company_name = CharField(max_length=25, null=True, blank=True, verbose_name='Название компании')
-    is_company = BooleanField(default=False, verbose_name='Признак компании')
+    company_data = OneToOneField('company.Company', on_delete=PROTECT, null=True, blank=True,
+                                 related_name='company_user', verbose_name='Компания')
 
     create_date = DateTimeField(auto_now_add=True, auto_now=False, null=True, blank=True, verbose_name='Создано')
     update_date = DateTimeField(auto_now_add=False, auto_now=True, verbose_name='Обновлено')
@@ -47,19 +47,11 @@ class User(AbstractUser, MPTTModel):
     def autocomplete_search_fields():
         return 'id', 'username', 'last_name', 'first_name', 'phone'
 
+    @property
+    def is_company(self):
+        if self.company_data:
+            return True
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-
-
-class Department(Model):
-    name = CharField(max_length=20, null=True, blank=True, verbose_name='Название')
-    company = ForeignKey('users.User', on_delete=PROTECT, related_name='company_department', blank=True,
-                         null=True, verbose_name='Компания')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name = 'Департамент'
-        verbose_name_plural = 'Департамент'

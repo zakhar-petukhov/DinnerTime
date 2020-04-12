e92f07056cdc86bb30880e4233c4e2452418172c
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-12 21:45:05 +0300

    :sparkles: create serializer for work method dish and dish group

--- dinner_time/apps/dinner/serializers.py
+++ dinner_time/apps/dinner/serializers.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
-from apps.dinner.models import Menu, Dish
+
+from apps.dinner.models import Menu, Dish, MenuGroup
 
 
 class DishSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dish
-        fields = ('name', 'cost', 'weight', 'composition', 'menu_group', 'added_dish')
+        fields = ('id', 'name', 'cost', 'weight', 'composition', 'menu_group', 'added_dish')
+
+
+class DishGroupSerializer(serializers.ModelSerializer):
+    dishes = DishSerializer(many=True)
+
+    class Meta:
+        model = MenuGroup
+        fields = ['id', 'name', 'dishes']
 
 
 class MenuSerializer(serializers.ModelSerializer):
@@ -13,4 +22,4 @@ class MenuSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Menu
-        fields = ['dish', 'available_order_date', 'close_order_time']
+        fields = ['id', 'dish', 'available_order_date', 'close_order_time']

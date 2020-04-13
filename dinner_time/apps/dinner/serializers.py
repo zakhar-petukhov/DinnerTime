7a3052e7921f1bb9efc55cca7a84010172b7f342
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-13 21:43:36 +0300

    :construction: create menu sketches

--- dinner_time/apps/dinner/serializers.py
+++ dinner_time/apps/dinner/serializers.py
@@ -3,14 +3,22 @@ from rest_framework import serializers
 from apps.dinner.models import Menu, Dish, MenuGroup
 
 
+class RecursiveField(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
+
+
 class DishSerializer(serializers.ModelSerializer):
+    additional_dish = RecursiveField(many=True, required=False)
+
     class Meta:
         model = Dish
-        fields = ('id', 'name', 'cost', 'weight', 'composition', 'menu_group', 'added_dish')
+        fields = ('id', 'name', 'cost', 'weight', 'composition', 'menu_group', 'added_dish', 'additional_dish')
 
 
 class DishGroupSerializer(serializers.ModelSerializer):
-    dishes = DishSerializer(many=True)
+    dishes = DishSerializer(many=True, required=False)
 
     class Meta:
         model = MenuGroup
--- dinner_time/apps/dinner/views.py
+++ dinner_time/apps/dinner/views.py
@@ -1,18 +1,63 @@
+from django.utils.decorators import method_decorator
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from apps.dinner.serializers import *
 
 
+@method_decorator(name='update', decorator=swagger_auto_schema(
+    operation_summary='Обновление данных блюда',
+    operation_description='''
+''',
+    request_body=DishSerializer,
+    responses={
+        '200': openapi.Response('Успешно', DishSerializer),
+        '400': 'Неверный формат запроса'
+    }
+)
+                  )
+@method_decorator(name='create', decorator=swagger_auto_schema(
+    operation_summary='Создание блюда',
+    operation_description='''
+При создании блюда указывается все важные параметры:
+1) Название
+2) Стоимость
+3) Вес
+4) Состав
+Также можно сразу прикрепить к определенной категорий блюд
+''',
+    request_body=DishSerializer,
+    responses={
+        '201': openapi.Response('Создано', DishSerializer),
+        '400': 'Неверный формат запроса'
+    }
+)
+                  )
+@method_decorator(name='list', decorator=swagger_auto_schema(
+    operation_summary='Получение всех блюд',
+    request_body=DishSerializer,
+    responses={
+        '200': openapi.Response('Успешно', DishSerializer),
+        '400': 'Неверный формат запроса'
+    }
+)
+                  )
 class DishViewSet(ModelViewSet):
     permission_classes = [AllowAny]
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
-    lookup_field = "dish_id"
+
+    def get_object(self):
+        return get_object_or_404(Dish, id=self.kwargs.get("dish_id"))
 
 
 class DishGroupViewSet(ModelViewSet):
     permission_classes = [AllowAny]
     queryset = MenuGroup.objects.all()
     serializer_class = DishGroupSerializer
-    lookup_field = "dish_group_id"
+
+    def get_object(self):
+        return get_object_or_404(MenuGroup, id=self.kwargs.get("dish_group_id"))

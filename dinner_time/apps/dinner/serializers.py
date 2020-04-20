75f4d0ab0b17adce1a2fb67ec831442ea07fa622
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-04-20 21:40:53 +0300

    :art: improving structure for add side dish to dish

--- dinner_time/apps/dinner/serializers.py
+++ dinner_time/apps/dinner/serializers.py
@@ -1,20 +1,28 @@
 from rest_framework import serializers
 
 from apps.dinner.models import *
-from apps.dinner.utils import create_additional_dish
+from apps.dinner.utils import create_additional_dish, create_additional_dish_for_complex
 
 
 class AddedDishSerializer(serializers.ModelSerializer):
-    name = serializers.ReadOnlyField(source='from_dish.name')
+    """
+    Serializer for add dish to dish
+    """
+
+    name = serializers.CharField(source='from_dish.name')
+    cost = serializers.FloatField(source='from_dish.cost')
+    weight = serializers.FloatField(source='from_dish.weight')
+    composition = serializers.CharField(source='from_dish.composition')
+    category_dish = serializers.IntegerField(source='from_dish.category_dish.id')
 
     class Meta:
         model = AddedDish
-        fields = ('id', 'name', 'for_complex', 'сomplex_dinner')
+        fields = ('id', 'name', 'cost', 'weight', 'composition', 'category_dish', 'for_complex', 'сomplex_dinner')
 
 
 class DishSerializer(serializers.ModelSerializer):
     """
-    Serializer for dish with update method
+    Serializer for dish with create and update method
     """
 
     id = serializers.IntegerField(read_only=False, required=False)
@@ -22,33 +30,26 @@ class DishSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Dish
-        fields = ('id', 'name', 'cost', 'weight', 'composition', 'menu_group', 'added_dish')
+        fields = ('id', 'name', 'cost', 'weight', 'composition', 'category_dish', 'added_dish')
 
     def get_added_dish(self, obj):
         is_complex = self.context.get("for_complex", False)
-        qs = AddedDish.objects.filter(to_dish=obj, for_complex=is_complex)
+        complex_id = self.context.get("complex_id", None)
+
+        qs = AddedDish.objects.filter(to_dish=obj, for_complex=is_complex, сomplex_dinner=complex_id)
         return [AddedDishSerializer(m).data for m in qs]
 
     def create(self, validated_data):
         validated_data.pop('added_dish', None)
         create_dish = Dish.objects.create(**validated_data)
-
-        if self.initial_data.get("added_dish"):
-            for dish in self.initial_data.get("added_dish"):
-                dish_id = dish.get("id")
-                AddedDish.objects.get_or_create(to_dish=create_dish, from_dish=Dish.objects.get(pk=dish_id))
-
+        dishes = self.initial_data.get("added_dish", [])
+        create_additional_dish(dishes, create_dish)
         return create_dish
 
     def update(self, instance, validated_data):
         dishes = self.initial_data.get("added_dish", [])
-
-        for dish in dishes:
-            dish_id = dish.get("id")
-
-            AddedDish.objects.get_or_create(to_dish=instance, from_dish=Dish.objects.get(pk=dish_id))
-
-        instance.menu_group = validated_data.pop('menu_group', None)
+        create_additional_dish(dishes, instance)
+        instance.category_dish = validated_data.pop('category_dish', instance.category_dish)
         instance.__dict__.update(**validated_data)
         instance.save()
         return instance
@@ -59,32 +60,33 @@ class ComplexDinnerSerializer(serializers.ModelSerializer):
     Serializer for complex dinner with create and update method
     """
 
-    dishes = DishSerializer(many=True, required=False)
+    dishes = serializers.SerializerMethodField()
 
     class Meta:
         model = ComplexDinner
         fields = ['id', 'name', 'dishes']
 
+    def get_dishes(self, obj):
+        self.context["complex_id"] = obj.id
+        serializer = DishSerializer(many=True, required=False, context=self.context, data=obj.dishes)
+        serializer.is_valid()
+        return serializer.data
+
     def create(self, validated_data):
         validated_data.pop("dishes", None)
-        dishes = self.initial_data.get("dishes", [])
-
         complex_dinner = ComplexDinner.objects.create(**validated_data)
-        create_additional_dish(dishes, complex_dinner)
+        create_additional_dish_for_complex(self.initial_data.get("dishes", []), complex_dinner)
         return complex_dinner
 
     def update(self, instance, validated_data):
         validated_data.pop("dishes", None)
-        dishes = self.initial_data.get("dishes")
-
-        create_additional_dish(dishes, instance)
-
+        create_additional_dish_for_complex(self.initial_data.get("dishes", []), instance)
         instance.__dict__.update(**validated_data)
         instance.save()
         return instance
 
 
-class DishGroupSerializer(serializers.ModelSerializer):
+class DishCategorySerializer(serializers.ModelSerializer):
     """
     Serializer for category dish
     """
@@ -92,7 +94,7 @@ class DishGroupSerializer(serializers.ModelSerializer):
     dishes = DishSerializer(many=True, required=False)
 
     class Meta:
-        model = MenuGroup
+        model = CategoryDish
         fields = ['id', 'name', 'dishes']
 
 

fd281cd24f7f04653c3f0b07f7660d2e00b05018
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-06-19 11:24:29 +0300

    :pencil: add req func for swagger

--- dinner_time/apps/dinner/data_for_swagger.py
+++ dinner_time/apps/dinner/data_for_swagger.py
@@ -43,7 +43,8 @@ request_for_create_menu = Schema(type=TYPE_OBJECT,
                                                                           type=TYPE_STRING)),
                                      'available_order_date': Schema(type=TYPE_STRING,
                                                                     format="date",
-                                                                    title='Дата, на которую создано меню')
+                                                                    title='Дата, на которую создано меню'),
+                                     'number_day': Schema(type=TYPE_INTEGER)
                                  })
 
 request_for_create_dinner = Schema(type=TYPE_OBJECT,
@@ -53,3 +54,30 @@ request_for_create_dinner = Schema(type=TYPE_OBJECT,
                                                                     type=TYPE_STRING)),
                                        'date_action_begin': Schema(type=TYPE_STRING, title='Заказ на дату')
                                    })
+
+request_for_week_menu = Schema(type=TYPE_OBJECT,
+                               properties={
+                                   'dishes': Schema(type=TYPE_ARRAY, title='Блюда',
+                                                    items=Items(enum={'id': TYPE_INTEGER},
+                                                                type=TYPE_STRING)),
+
+                               })
+
+request_for_remove_day_menu_from_week_menu = Schema(type=TYPE_OBJECT,
+                                                    properties={
+                                                        'dishes': Schema(type=TYPE_ARRAY, title='Блюда',
+                                                                         items=Items(enum={'id': TYPE_INTEGER,
+                                                                                           'remove': TYPE_BOOLEAN},
+                                                                                     type=TYPE_STRING)),
+
+                                                    })
+
+request_for_template = Schema(type=TYPE_OBJECT,
+                              properties={
+                                  'name': Schema(type=TYPE_STRING, title='Название шаблона'),
+                                  'number_week': Schema(type=TYPE_INTEGER, title='Номер недели'),
+                                  'menu': Schema(type=TYPE_OBJECT, title='Меню',
+                                                 properties={
+                                                     'id': Schema(type=TYPE_INTEGER, title='id недельного меню')
+                                                 })
+                              })
